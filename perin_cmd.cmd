e093969515b611c69893f6c84cb0e38ede0fd65e
João Victor Perin <joaovperin@live.com>
2019-02-27 20:25:22 -0300

    Update perin_cmd.cmd
    Un-hardcoded user directory so it can be used by others

--- perin_cmd.cmd
+++ perin_cmd.cmd
@@ -1,8 +1,13 @@
+rem Saves old path for restoring later
 @set OLD_PATH=%PATH%
 
-@set PATH=%PATH%;C:\Users\0199831\Downloads\VSCode-win32-x64-1.31.1\bin
-@set PATH=%PATH%;C:\Users\0199831\Downloads\PortableGit;
+rem Updates script PATH temporarily
+@set PATH=%PATH%;%USERPROFILE%\Downloads\VSCode-win32-x64-1.31.1\bin
+@set PATH=%PATH%;%USERPROFILE%\Downloads\PortableGit;
+@set PATH=%PATH%;%USERPROFILE%\Downloads\PortableGit\bin;
 
+rem Execute the script with Windows own command prompt
 @call C:\Windows\System32\cmd.exe %*
 
+rem Restores the path value
 @set PATH=%OLD_PATH%
