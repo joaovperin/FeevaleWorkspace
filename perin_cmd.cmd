2248b70f3fa66c2aa78dd77f88ba8fe0f6bf9461
João Victor Antunes Perin <0199831@feevale.br>
2019-02-27 20:20:31 -0300

    Created some shortcuts for Fours prompts and a custom path-setter cmd wrapper.

---
+++ perin_cmd.cmd
@@ -0,0 +1,8 @@
+@set OLD_PATH=%PATH%
+
+@set PATH=%PATH%;C:\Users\0199831\Downloads\VSCode-win32-x64-1.31.1\bin
+@set PATH=%PATH%;C:\Users\0199831\Downloads\PortableGit;
+
+@call C:\Windows\System32\cmd.exe %*
+
+@set PATH=%OLD_PATH%
